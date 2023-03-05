bc2c9ba3532ed37d7b5435b943180d37cd640da8
bacng <nhb.fit.hut@gmail.com>
2023-03-05 23:05:57 +0700

    update architect structurer app

--- main.go
+++ main.go
@@ -2,30 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 var portZNumber = ":8088"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-
-	renderTemplates(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "about.page.tmpl")
-
-}
-
-func renderTemplates(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing tamplate: ", err)
-		return
-	}
-}
 func main() {
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	n, err := fmt.Fprintf(w, "Hello World")
