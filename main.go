a50d30d00ff6299870303d63cd7acb43a5462b70
bacng <nhb.fit.hut@gmail.com>
2023-03-05 21:51:21 +0700

    handler functions

--- main.go
+++ main.go
@@ -5,14 +5,33 @@ import (
 	"net/http"
 )
 
+var portZNumber = ":8088"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the homepage")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(10, 20)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the About page and 10 + 20 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	var sum int
+	sum = x + y
+	return sum
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("NUmber of bytes written: %d", n))
-	})
-	_ = http.ListenAndServe(":8088", nil)
-	// fmt.Println("hêlllo golang")
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	n, err := fmt.Fprintf(w, "Hello World")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(fmt.Sprintf("NUmber of bytes written: %d", n))
+	// })
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	_ = http.ListenAndServe(portZNumber, nil)
 }
