26f934bf738af8899eb045a23ccc92c95dd2e243
bacng <nhb.fit.hut@gmail.com>
2023-03-07 22:33:02 +0700

    optimizing our template cache by using an application config

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -70,8 +70,13 @@ func NewTemplates(a *config.AppConfig) {
 }
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	// get the template cache from the app config
-	tc := app.TemplateCache
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
 
 	//create a template cache
 
